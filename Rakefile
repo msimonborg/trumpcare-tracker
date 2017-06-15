b38aaed8d9d49f2206d2fb8a671265cbe8571c91
M. Simon Borg <msimonborg@gmail.com>
2017-06-15 03:37:16 -0400

    finished for now

--- Rakefile
+++ Rakefile
@@ -1,8 +1,67 @@
 # frozen_string_literal: true
 
 require 'bundler/gem_tasks'
+require 'csv'
+require 'pyr'
+require 'trumpcare_tracker'
 
 require 'rubocop/rake_task'
 RuboCop::RakeTask.new
 
 task default: %i[spec rubocop]
+
+desc 'Output a report of Senate Democrats Trumpcare tweets'
+task :export do
+  reps = PYR.reps do |r|
+    r.party = 'democrat'
+    r.chamber = 'upper'
+  end
+
+  csv_report = CSV.generate do |csv|
+    csv << %w[
+        senator
+        user_name
+        tweets_in_last_seven_days
+        trumpcare_tweets
+        tct_percentage
+        russia_tweets
+        rt_percentage
+        tct_to_rt_ratio
+        trumpcare_tweet_urls
+        russia_tweet_urls
+      ]
+    audit_tweets(reps) { |tracker| csv << tracker.to_h.values }
+  end
+
+  File.open('trumpcare_tweet_report.csv', 'w') do |file|
+    file.write csv_report
+  end
+end
+
+desc 'Audit and tweet results'
+task :tweet do
+  reps = PYR.reps do |r|
+    r.party   = 'democrat'
+    r.chamber = 'upper'
+  end
+
+  audit_tweets(reps) do |tracker, rep|
+    tracker.to_tweet
+    tracker.client.update(
+      "#{rep.official_full}\n"\
+      "#{rep.office_locations.map { |off| "#{off.city} - #{off.phone}" }.join("\n")}"
+    )
+  end
+end
+
+def audit_tweets(reps)
+  reps.objects.each_with_index do |rep, i|
+    puts "Sending requests to Twitter API for #{rep.official_full}"
+    tracker = TrumpcareTracker.new(rep.twitter)
+    next if tracker.user.screen_name.downcase != rep.twitter.downcase
+    tracker.audit
+    yield(tracker, rep) if block_given?
+    puts "#{i + 1} down. Pausing for 45 seconds to avoid hitting API rate limit."
+    sleep(45)
+  end
+end
--- lib/trumpcare_tracker.rb
+++ lib/trumpcare_tracker.rb
@@ -28,57 +28,111 @@ class TrumpcareTracker
   end
 
   def timeline
-    @_timeline ||= client.user_timeline(user, exclude_replies: true, count: 200)
+    @_timeline ||= fetch_timeline
+  end
+
+  # Make two cursored API calls to fetch the 400 most recent tweets
+  def fetch_timeline
+    timeline = client.user_timeline(user, exclude_replies: true, count: 200)
+    timeline + client.user_timeline(
+      user,
+      exclude_replies: true,
+      max_id: timeline.last.id - 1,
+      count: 200
+    )
   end
 
   # Collect a user's tweets within the last 7 days
   def recent_tweets
-    timeline.each_with_object([]) do |tweet, memo|
+    @_recent_tweets ||= timeline.each_with_object([]) do |tweet, memo|
       age_of_tweet_in_days = (Time.now.to_date - tweet.created_at.to_date).to_i
       memo << tweet if age_of_tweet_in_days <= 7
     end
   end
 
-  def search_for_keywords(regex)
+  def reduce_by_keywords(regex)
     recent_tweets.each_with_object([]) do |tweet, memo|
       full_text = client.status(tweet.id, tweet_mode: 'extended').attrs[:full_text]
       memo << tweet if full_text.downcase.match?(regex)
     end
   end
 
-  def healthcare_tweets
-    search_for_keywords(healthcare_keyword_regex)
+  def trumpcare_tweets
+    @_trumpcare_tweets ||= reduce_by_keywords(trumpcare_keyword_regex)
   end
 
-  def healthcare_keyword_regex
+  def trumpcare_keyword_regex
     /(ahca|trumpcare|healthcare|health|care|drug|medication|prescription|vaccine)/
   end
 
   def russia_tweets
-    search_for_keywords(russia_keyword_regex)
+    @_russia_tweets ||= reduce_by_keywords(russia_keyword_regex)
   end
 
   def russia_keyword_regex
-    /(russia|comey|sessions|mueller|fbi|flynn|obstruction of justice|collusion)/
+    /(russia|comey|sessions|mueller|fbi|flynn|obstruction of justice|collusion|putin|kremlin)/
   end
 
   def audit
-    recent_tweets_count = recent_tweets.count
-    healthcare_tweets_count = healthcare_tweets.count
-    russia_tweets_count = russia_tweets.count
-    puts user.name
-    puts recent_tweets_count
-    puts "#{healthcare_tweets_count}, #{percentage(healthcare_tweets_count, recent_tweets_count)}%"
-    puts "#{russia_tweets_count}, #{percentage(russia_tweets_count, recent_tweets_count)}%"
-    puts "#{ratio(healthcare_tweets_count, russia_tweets_count)}"\
-      ' health care tweets for ever Russia tweet.'
+    puts to_s
+  end
+
+  def to_s
+    @_to_s ||= "@#{user.screen_name}'s last 7 days\n#{recent_tweets_count} tweets\n"\
+      "#{trumpcare_tweets_count} TrumpCare - #{trumpcare_tweets_percentage}%\n"\
+      "#{russia_tweets_count} Russia - #{russia_tweets_percentage}%\n"\
+      "#{trumpcare_to_russia_tweets_ratio} TrumpCare tweets for every Russia tweet"
+  end
+
+  def recent_tweets_count
+    @_recent_tweets_count ||= recent_tweets.count
+  end
+
+  def trumpcare_tweets_count
+    @_trumpcare_tweets_count ||= trumpcare_tweets.count
+  end
+
+  def russia_tweets_count
+    @_russia_tweets_count ||= russia_tweets.count
+  end
+
+  def trumpcare_tweets_percentage
+    percentage(trumpcare_tweets.count, recent_tweets.count)
+  end
+
+  def russia_tweets_percentage
+    percentage(russia_tweets.count, recent_tweets.count)
+  end
+
+  def trumpcare_to_russia_tweets_ratio
+    ratio(trumpcare_tweets.count, russia_tweets.count)
   end
 
   def ratio(numerator, denominator)
+    return 0.0 if denominator.zero?
     (numerator / denominator.to_f).round(4)
   end
 
   def percentage(numerator, denominator)
     (ratio(numerator, denominator) * 100).round(2)
+  end
+
+  def to_h
+    {
+      senator: user.name,
+      user_name: user.screen_name,
+      tweets_in_last_seven_days: recent_tweets.count,
+      trumpcare_tweet_count: trumpcare_tweets.count,
+      tct_percentage: trumpcare_tweets_percentage,
+      russia_tweet_count: russia_tweets.count,
+      rt_percentage: russia_tweets_percentage,
+      tct_to_rt_ratio: trumpcare_to_russia_tweets_ratio,
+      trumpcare_tweet_urls: trumpcare_tweets.map { |tweet| tweet.uri.to_s },
+      russia_tweet_urls: russia_tweets.map { |tweet| tweet.uri.to_s }
+    }
+  end
+
+  def to_tweet
+    client.update(".#{to_s}")
   end
 end
