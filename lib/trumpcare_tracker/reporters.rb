d68e2ebf131c093519f5485a775700c901d9a698
M. Simon Borg <msimonborg@gmail.com>
2017-06-17 00:35:17 -0400

    tasks for democrats and republicans

--- lib/trumpcare_tracker/reporters.rb
+++ lib/trumpcare_tracker/reporters.rb
@@ -22,29 +22,40 @@ class TrumpcareTracker
 
     def export_task
       namespace :tracker do
-        desc 'Output a report of Senate Democrats Trumpcare tweets'
-        task :export do
-          csv_tweet_report = CSV.generate do |csv|
-            csv << %w[
-              senator
-              official_user_name
-              alt_user_name
-              tweets_in_last_seven_days
-              trumpcare_tweets
-              tct_percentage
-              russia_tweets
-              rt_percentage
-              tct_to_rt_ratio
-              trumpcare_tweet_urls
-              russia_tweet_urls
-            ]
-            audit_tweets(democrats) { |tracker| csv << tracker.to_h.values }
+        namespace :export do
+          desc 'Output a report of Senate Democrats Trumpcare tweets'
+          task :democrats do
+            export(:democrats)
           end
 
-          File.open('trumpcare_tweet_report_republicans.csv', 'w') do |file|
-            file.write csv_tweet_report
+          desc 'Output a report of Senate Republicans Trumpcare tweets'
+          task :republicans do
+            export(:republicans)
           end
         end
+      end
+    end
+
+    def export(caucus)
+      csv_tweet_report = CSV.generate do |csv|
+        csv << %w[
+          senator
+          official_user_name
+          alt_user_name
+          tweets_in_last_seven_days
+          trumpcare_tweets
+          tct_percentage
+          russia_tweets
+          rt_percentage
+          tct_to_rt_ratio
+          trumpcare_tweet_urls
+          russia_tweet_urls
+        ]
+        audit_tweets(send(caucus)) { |tracker| csv << tracker.to_h.values }
+      end
+
+      File.open("trumpcare_tweet_report_#{caucus}.csv", 'w') do |file|
+        file.write csv_tweet_report
       end
     end
 
@@ -53,28 +64,40 @@ class TrumpcareTracker
     def homepage_scraper_task
       namespace :tracker do
         desc 'Search Senators\' official homepage for TrumpCare and Russia Mentions'
-        task :homepage_scraper do
-          csv_homepage_report = CSV.generate do |csv|
-            csv << %w[senator url trumpcare_mentions russia_mentions trumpcare_to_russia_ratio]
-            democrats.each do |rep|
-              doc = Nokogiri::HTML(open(rep.url))
-              trumpcare_mentions_count = mentions_mapper(doc, TrumpcareTracker.trumpcare_keyword_regex).count
-              russia_mentions_count = mentions_mapper(doc, TrumpcareTracker.russia_keyword_regex).count
-              csv << [
-                  rep.official_full,
-                  rep.url,
-                  trumpcare_mentions_count,
-                  russia_mentions_count,
-                  TrumpcareTracker.ratio(trumpcare_mentions_count, russia_mentions_count)
-              ]
-              puts "Scraped #{rep.official_full}'s homepage"
-            end
+        namespace :homepage_scraper do
+          desc 'Search Democrat\'s homepages'
+          task :democrats do
+            homepage_scraper(:democrats)
           end
 
-          File.open('trumpcare_homepage_report.csv', 'w') do |file|
-            file.write csv_homepage_report
+          desc 'Search Republican\'s homepages'
+          task :republicans do
+            homepage_scraper(:republicans)
           end
         end
+      end
+    end
+
+    def homepage_scraper(caucus)
+      csv_homepage_report = CSV.generate do |csv|
+        csv << %w[senator url trumpcare_mentions russia_mentions trumpcare_to_russia_ratio]
+        send(caucus).each do |rep|
+          doc = Nokogiri::HTML(open(rep.url))
+          trumpcare_mentions_count = mentions_mapper(doc, TrumpcareTracker.trumpcare_keyword_regex).count
+          russia_mentions_count = mentions_mapper(doc, TrumpcareTracker.russia_keyword_regex).count
+          csv << [
+            rep.official_full,
+            rep.url,
+            trumpcare_mentions_count,
+            russia_mentions_count,
+            TrumpcareTracker.ratio(trumpcare_mentions_count, russia_mentions_count)
+          ]
+          puts "Scraped #{rep.official_full}'s homepage"
+        end
+      end
+
+      File.open("trumpcare_homepage_report_#{caucus}.csv", 'w') do |file|
+        file.write csv_homepage_report
       end
     end
   end
--- lib/trumpcare_tracker/tweet_bot.rb
+++ lib/trumpcare_tracker/tweet_bot.rb
@@ -14,37 +14,55 @@ class TrumpcareTracker
   class TweetBot < RakeTask
     include RakeTask::Methods
 
+    attr_reader :screen_name, :first_tweet_block
+
     def initialize(screen_name, &first_tweet_block)
+      @screen_name       = screen_name
+      @first_tweet_block = first_tweet_block
+      tweet_bot_task
+    end
+
+    def tweet_bot_task
       namespace(:tracker) do
-        desc 'Audit Trumpcare tweet activity and post a thread of updates'
-
-        task(:tweet_bot) do
-          tracker = TrumpcareTracker.new('TrumpCareTracker', screen_name)
-          tweet = Twitter::Tweet.new(id: nil)
-          first_tweet = tracker.client.update(first_tweet_block.call) if block_given?
-          democrats.each_with_index do |rep, i|
-            next if rep.twitter.nil?
-            reply_to_tweet = if i.zero?
-                               first_tweet
-                             else
-                               tweet
-                             end
-
-            begin
-              tweet = post(rep, i, reply_to_tweet)
-            rescue Twitter::Error => e
-              puts e.message
-              puts 'Waiting 5 minutes to see if the issue can be resolved.'
-              sleep(300)
-              tweet = post(rep, i, reply_to_tweet)
-            rescue Twitter::Error => e
-              puts e.message
-              puts 'Waiting 5 more minutes to try one more time. '\
-                'If there\'s another exception I\'ll let it fail'
-              sleep(300)
-              tweet = post(rep, i, reply_to_tweet)
-            end
+        namespace(:tweet_bot) do
+          desc 'Audit Democrats Trumpcare tweet activity and post a thread of updates'
+          task(:democrats) do
+            tweet_bot(:democrats)
           end
+
+          desc 'Audit Republicans Trumpcare tweet activity and post a thread of updates'
+          task(:republicans) do
+            tweet_bot(:republicans)
+          end
+        end
+      end
+    end
+
+    def tweet_bot(caucus)
+      tracker = TrumpcareTracker.new('TrumpCareTracker', screen_name)
+      tweet = Twitter::Tweet.new(id: nil)
+      first_tweet = tracker.client.update(first_tweet_block.call) if block_given?
+      send(caucus).each_with_index do |rep, i|
+        next if rep.twitter.nil?
+        reply_to_tweet = if i.zero?
+                           first_tweet
+                         else
+                           tweet
+                         end
+
+        begin
+          tweet = post(rep, i, reply_to_tweet)
+        rescue Twitter::Error => e
+          puts e.message
+          puts 'Waiting 5 minutes to see if the issue can be resolved.'
+          sleep(300)
+          tweet = post(rep, i, reply_to_tweet)
+        rescue Twitter::Error => e
+          puts e.message
+          puts 'Waiting 5 more minutes to try one more time. '\
+                'If there\'s another exception I\'ll let it fail'
+          sleep(300)
+          tweet = post(rep, i, reply_to_tweet)
         end
       end
     end
